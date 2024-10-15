16bf11dd1f385b9215fdb50a15d1276c7afdfd6d
okunosh <uyosin0kuo1@gmail.com>
2024-10-15 23:17:14 +0900

    Authentication fun installment completed. But header name is not displayed in Top Page

--- resources/views/top_page.blade.php
+++ resources/views/top_page.blade.php
@@ -6,39 +6,49 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Top Page</title>
     </head>
-    <body>
-        <header>
-            {{--
-            <h1 class="headline">
-                <div class='post'>
-                    <a>Name:{{$users->user_name}}</a>
-                </div>
-            </h1>
-            --}}
-            <div class="navigation">
-                <nav>
-                    <ul>
-                        <li>Make New Project</li>
-                        <li>Notes</li>
-                        <li>Achivements</li>
-                    </ul>
-                </nav>
-            </div>
-        </header>
 
-        <article>
-            <div class="content">Calendar</div>
-            <div class="sides">
-                @foreach ($lgoals as $lgoal)
-                    <div class="goals">
-                    <h2 class="long_goals">{{ $lgoal->goal_name }}</h2>
-                    <p>{{ $lgoal->goal_description }}</p>
-                    <p>進捗度：</p>
+    <x-app-layout>
+        <x-slot name="header">
+            Top Page
+        </x-slot>
+        <body>
+            <!--
+            <header>
+                {{--
+                <h1 class="headline">
+                    <div class='post'>
+                        <a>Name:{{$users->user_name}}</a>
                     </div>
-                @endforeach
-            </div> 
-            <div class="subcontent">To Do List</div>
-        </article>
+                </h1>
+                --}}
+                <div class="navigation">
+                    <nav>
+                        <ul>
+                            <li>Make New Project</li>
+                            <li>Notes</li>
+                            <li>Achivements</li>
+                        </ul>
+                    </nav>
+                </div>
+            </header>
+            -->
+            
+            <article>
+            <h2>you are : {{ Auth::user()->user_name }}</h2>
+                <div class="content">Calendar</div>
+                <div class="sides">
+                    @foreach ($lgoals as $lgoal)
+                        <div class="goals">
+                        <h2 class="long_goals">{{ $lgoal->goal_name }}</h2>
+                        <p>{{ $lgoal->goal_description }}</p>
+                        <p>進捗度：</p>
+                        </div>
+                    @endforeach
+                </div> 
+                <div class="subcontent">To Do List</div>
+            </article>
+        </body>
+    </x-app-layout>
+
     
-    </body>
 </html>
